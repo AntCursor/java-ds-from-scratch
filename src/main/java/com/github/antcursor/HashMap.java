3bf6b33e7c874b4828a799fd8b19e14411f80b02
AntCursor <antcursor@gmail.com>
2026-08-16 22:58:24 -0300

    chore: added the hasmap class and TODOs for its basic methods

---
+++ src/main/java/com/github/antcursor/HashMap.java
@@ -0,0 +1,51 @@
+package com.github.antcursor;
+
+/**
+ * Simple Hash Map implementation using the hashCode method
+ * of all {@link Object} classes.
+ */
+public class HashMap<K, V> {
+  public boolean isEmpty() {
+    // TODO: implement method
+    return false;
+  }
+
+  public int size() {
+    // TODO: implement method
+    return 0;
+  }
+
+  public V put(K key, V value) {
+    // TODO: implement method
+    return null;
+  }
+
+  public V get(K key) {
+    // TODO: implement method
+    return null;
+  }
+
+  public V remove(K key) {
+    // TODO: implement method
+    return null;
+  }
+
+  public boolean remove(K key, V value) {
+    // TODO: implement method
+    return false;
+  }
+
+  public boolean containsKey(K key) {
+    // TODO: implement method
+    return false;
+  }
+
+  public boolean containsValue(V value) {
+    // TODO: implement method
+    return false;
+  }
+
+  public void clear() {
+    // TODO: implement method
+  }
+}
